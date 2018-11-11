025882b62ab88415524a7847f8caa92826fbe4f8
bobich88 <44919201+bobich88@users.noreply.github.com>
2018-11-11 11:48:48 +0300

    new function

--- User.php
+++ User.php
@@ -3,4 +3,12 @@
 class User
 {
     public $name;
+    public $role;
+
+
+    function __construct($name, $role)
+    {
+        $this->name = $name;
+        $this->role = $role;
+    }
 }
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'User.php';
+
+$user = new User('Vasia', 2);
+// $user->name = 'Artem';
+
+
+var_dump($user);
